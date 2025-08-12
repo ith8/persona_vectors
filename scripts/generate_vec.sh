87f2b35c4c927e362057c1c82796c1c5f8ff44de
Thee Ho <thee@berkeley.edu>
2025-08-12 19:33:29 +0100

    update script

--- scripts/generate_vec.sh
+++ scripts/generate_vec.sh
@@ -1,7 +1,7 @@
 gpu=${1:-0}
 
 
-CUDA_VISIBLE_DEVICES=$gpu python -m eval.eval_persona \
+CUDA_VISIBLE_DEVICES=0 python -m eval.eval_persona \
     --model Qwen/Qwen2.5-7B-Instruct \
     --trait sycophantic \
     --output_path eval_persona_extract/Qwen2.5-7B-Instruct/sycophantic_pos_instruct.csv \
@@ -10,7 +10,7 @@ CUDA_VISIBLE_DEVICES=$gpu python -m eval.eval_persona \
     --judge_model gpt-4.1-mini-2025-04-14 \
     --version extract
 
-CUDA_VISIBLE_DEVICES=$gpu python -m eval.eval_persona \
+CUDA_VISIBLE_DEVICES=0 python -m eval.eval_persona \
     --model Qwen/Qwen2.5-7B-Instruct \
     --trait sycophantic \
     --output_path eval_persona_extract/Qwen2.5-7B-Instruct/sycophantic_neg_instruct.csv \
@@ -19,7 +19,7 @@ CUDA_VISIBLE_DEVICES=$gpu python -m eval.eval_persona \
     --judge_model gpt-4.1-mini-2025-04-14 \
     --version extract
 
-CUDA_VISIBLE_DEVICES=$gpu python generate_vec.py \
+CUDA_VISIBLE_DEVICES=0 python generate_vec.py \
     --model_name Qwen/Qwen2.5-7B-Instruct \
     --pos_path eval_persona_extract/Qwen2.5-7B-Instruct/sycophantic_pos_instruct.csv \
     --neg_path eval_persona_extract/Qwen2.5-7B-Instruct/sycophantic_neg_instruct.csv \
